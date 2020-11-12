c406424311a4f2d91a5c146b9301035d5e3a96f5
kraftl-UL <71893133+kraftl-UL@users.noreply.github.com>
2020-11-12 21:49:42 +0100

    Update postgres_db.py
    Use for first run this file. After the tbale has been created comment out the create and insert line (lines 13+15)

--- postgres/postgres_db.py
+++ postgres/postgres_db.py
@@ -10,13 +10,11 @@ con = psycopg2.connect(dbname=database, user=user, password=password, host=host,
 
 cur = con.cursor()
 
+cur.execute("CREATE TABLE jokes (ID SERIAL PRIMARY KEY, joke TEXT);")
 
-#cur.execute("CREATE TABLE jokes (ID SERIAL PRIMARY KEY, joke TEXT);")
-
-#cur.execute("insert into jokes (ID, joke) values (%s, %s)", (1, "Warum freut sich eine Blondine, wenn sie nach 6 Monaten ein Puzzle geschafft hat? - Wil auf der Verpackung steht: 2-4 Jahre.") )
+cur.execute("insert into jokes (ID, joke) values (%s, %s)", (1, "Warum freut sich eine Blondine, wenn sie nach 6 Monaten ein Puzzle geschafft hat? - Weil auf der Verpackung steht: 2-4 Jahre.") )
 
 #execute query
-
 cur.execute("select joke from jokes;")
 print ("This is my favorite joke:")
 print(cur.fetchall())
