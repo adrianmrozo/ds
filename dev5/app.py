c16ce5176e90a5a3c8055dcfeb9991428bf6d6da
kraftl-UL <71893133+kraftl-UL@users.noreply.github.com>
2021-01-08 23:06:30 +0100

    include post request + define app

--- dev5/app.py
+++ dev5/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, request
 
+
+app = Flask(__name__)
+
     
 @app.route('/predict/',methods=['GET','POST'])
-  def predict():
-    pred = "Make a prediction:"
+def predict():
+    if (request.method == "POST"):
+        pred = "Make a prediction:"
+        #How?
+        return pred
+    else:
+        print("hte request wasn't a post, but a get.")
+        return 
+    
     return pred	
     
 if __name__ == '__main__':
-  app.run(debug=True, port=8080)
+  app.run(debug=True)
