e8f306d7d04d40b29d9959d29345ee1bb6ab8f5b
adrianmrozo <72199535+adrianmrozo@users.noreply.github.com>
2021-01-24 00:30:04 +0100

    Revert "Workon"

--- src/postgres_db.py
+++ src/postgres_db.py
@@ -5,51 +5,47 @@
 #password = input("Insert a password for your database:") or "pgpass"
 
 
-host = "postgres"
+host = "127.0.0.1"
 database = "milestone5"
-port = "5432"
+port = "5005"
 user = "postgres"
 password = "pgpass"
 
 import psycopg2
 import numpy as np
 
+
 con = psycopg2.connect(dbname=database, user=user, password=password, host=host, port = port)
 cur = con.cursor()
 
 # create input data table
-cur.execute("CREATE TABLE input_data (ID SERIAL PRIMARY KEY, input_label TEXT, image TEXT, predicted_label TEXT);")
-con.close()
+cur.execute("CREATE TABLE input_data (ID SERIAL PRIMARY KEY, input_label TEXT, image TEXT);")
+
+
+#train the model and store it
+#also make it available in this script
+import main
+model = main.model
 
 
 #store test data, test label, prediction label
+from test import test_one
+test_data, test_label, pred_label = test_one(model)
 
-def savingtestresult(test_label, test_data, pred_label):
 
-    host = "postgres"
-    database = "milestone5"
-    port = "5432"
-    user = "postgres"
-    password = "pgpass"
+#load testdata into database input_data
+cur.execute("insert into input_data (ID, input_label, image) values (%s, %s, %s)", (1, test_label, str(test_data)) )
 
-    con = psycopg2.connect(dbname=database, user=user, password=password, host=host, port = port)
-    cur = con.cursor()
 
-    #savingtestresult.counter += 1
-    #load testdata into database input_data
-    cur.execute("insert into input_data (ID, input_label, image, predicted_label) values (%s, %s, %s, %s)", (1, test_label, str(test_data), pred_label))
+#execute query
+cur.execute("select * from input_data;")
+image1 = np.fromstring(cur.fetchall()[-1], dtype = int).reshape(32,32,3)
+print ("These are the inputs that have been tested so far:")
+print(cur.fetchall()[-1], image1)
 
-    #execute query
-    #cur.execute("select * from input_data;")
-    #image1 = np.fromstring(cur.fetchall()[-1], dtype = int).reshape(32,32,3)
-    #print ("These are the inputs that have been tested so far:")
-    #print(cur.fetchall()[-1], image1)
-
-    #cur.execute("select * from predictions;")
-    #print ("The CNN predicted the tested inputs to be:")
-    #print(cur.fetchall())
-    #commit data to db
-    con.commit()
-    con.close()
-#savingtestresult.counter = 0
-
+#cur.execute("select * from predictions;")
+#print ("The CNN predicted the tested inputs to be:")
+#print(cur.fetchall())
+#commit data to db
+con.commit()
+con.close()
