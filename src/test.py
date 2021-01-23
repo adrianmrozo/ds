42389e2c531c5ee5a46aa46640f50d6215731964
kraftl-UL <71893133+kraftl-UL@users.noreply.github.com>
2021-01-23 13:27:44 +0100

    delete un-used function test_one

--- src/test.py
+++ src/test.py
@@ -1,5 +1,3 @@
-# Source: https://gurus.pyimagesearch.com/lesson-sample-running-a-pre-trained-network/#. Import the necessary packages
-#from __future__ import print_function
 from keras.models import load_model
 from keras.datasets import cifar10
 import numpy as np
@@ -38,36 +36,6 @@ def testing(model_name, save_dir, batch_size, y_test):
 	 print("[INFO] predicted: {}, actual: {}".format(gtLabels[prediction],
 		gtLabels[testLabels[i]]))
 
-		
-def test_one(model):
- # initialize the ground-truth labels for the CIFAR-10 dataset
- gtLabels = ["airplane", "automobile", "bird", "cat", "deer", "dog", "frog", "horse", "ship", "truck"]
- 
-  # scale the data points into the range [0, 1]
- print("[INFO] sampling one image from CIFAR-10...")
- (testData, testLabels) = cifar10.load_data()[1]
- testData = testData.astype("float") / 255.0
- idxs = np.random.choice(testData.shape[0], size=(1,), replace=False)
- (testData, testLabels) = (testData[idxs], testLabels[idxs])
- testLabels = testLabels.flatten()
-
- # make predictions on the sample of testing data
- print("[INFO] predicting on testing data...")
- probs = model.predict(testData, batch_size=32)
- predictions = probs.argmax(axis=1)
- 
- # loop over each of the testing data points
- for (i, prediction) in enumerate(predictions):
-	 print("[INFO] predicted: {}, actual: {}".format(gtLabels[prediction], gtLabels[testLabels[i]]))
-	
- for (i, prediction) in enumerate(predictions):
-  pred_label=gtLabels[prediction]
-  test_label=gtLabels[testLabels[i]] 
- 
- plt.imshow(testData[0])
- plt.imsave('test.png', testData[0])
-	 
- return testData, test_label, pred_label, idxs[0]
 
  
  		
